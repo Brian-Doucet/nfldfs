8dece8015e47f7bfb909558277c80cf21b31096f
Brian Doucet <doucetba@gmail.com>
2020-02-25 16:24:32 -0500

    Added function to collect urls

--- nfl-dfs/game_data.py
+++ nfl-dfs/game_data.py
@@ -5,46 +5,20 @@ import requests
 import io
 import pandas as pd
 
+def seasons_to_search(season_from, week_from, season_to=None, week_to=None):
+    # If season_to not entered, only search for one season
+    season_to_range = season_to
+    week_to_range = week_to
 
+    if not season_to_range:
+            season_to_range = season_from
 
+    if not week_to_range:
+        week_to_range = week_from
 
-# Function to get historical NFL daily fantasy results
+    seasons = [s for s in range(season_from, season_to_range + 1)]
+    weeks = [w for w in range(week_from, week_to_range])
 
-def get_historical_results(season_start, season_end=None):
-    """Scrapes historical data for NFL daily fantasy
+    return seasons, weeks
 
-    Parameters:
-    season_start (int): The season_start argume
-    season_end (int): The season_end ar
-
-    Returns:
-    int:Returning value
-
-   """
-
-    # If season_end is not entered, return the last full completed season
-    season_end_range = season_end
-
-    if not season_end_range:
-        season_end_range = int(date.today().year - 1)
-
-    seasons = [s for s in range(season_start, season_end_range + 1)]
-    weeks = [w for w in range(1, 2 + 1)]
-
-    # Initialize empty DataFrame to store the results
-    all_data = pd.DataFrame()
-
-    # Send the request and store the response
-    for s, w in itertools.product(seasons, weeks):
-        url = f"http://rotoguru1.com/cgi-bin/fyday.pl?week={w}&year={s}&game=dk&scsv=1"
-        r = requests.get(url).text
-
-        # Parse the data and append to all_data
-        soup = BeautifulSoup(r, "lxml")
-        data_string = io.StringIO(soup.find("pre").text)
-        data = pd.read_csv(data_string, sep=';')
-
-        all_data = pd.concat(objs=[all_data, data])
-
-    return(all_data)
-           
+#s = seasons_to_search(2014, 1)
