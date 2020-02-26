bf18ef9304783ed20d9c792cec212dee2572903b
Brian <doucetba@gmail.com>
2020-02-26 14:07:49 -0500

    updated function

--- nfl-dfs/game_data.py
+++ nfl-dfs/game_data.py
@@ -1,12 +1,7 @@
-from datetime import date
 import itertools
-from bs4 import BeautifulSoup
-import requests
-import io
-import pandas as pd
 
-def seasons_to_search(season_from, week_from, season_to=None, week_to=None):
-    # If season_to not entered, only search for one season
+def games_to_search(season_from, week_from, season_to=None, week_to=None):
+    
     season_to_range = season_to
     week_to_range = week_to
 
@@ -20,6 +15,6 @@ def seasons_to_search(season_from, week_from, season_to=None, week_to=None):
     weeks = [w for w in range(week_from, week_to_range + 1)]
 
     base_url = "http://rotoguru1.com/cgi-bin/fyday.pl?week={}&year={}&game=dk&scsv=1"
-    game_urls = [base_url.format(s, w) for s, w in itertools.product(seasons, weeks)]
+    game_urls = [base_url.format(s, w) for s, w in itertools.product(weeks, seasons)]
 
     return game_urls
