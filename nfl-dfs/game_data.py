bc30046d3bdf5b60b3f0f51aa41dfdfc0c9ecca9
Brian <doucetba@gmail.com>
2020-03-02 15:50:30 -0500

    Added docstring for games_to_search function

--- nfl-dfs/game_data.py
+++ nfl-dfs/game_data.py
@@ -7,6 +7,30 @@ from urllib.parse import urlparse
 
 # Function to create game search parameters
 def games_to_search(dfs_site, season_from, week_from, season_to=None, week_to=None):
+    """Returns a list of URLs.
+
+    Use this function to generate a list of URLs that can be used to fetch
+    NFL daily fantasy results for DraftKings, FanDuel, and Yahoo! Fantasy Sports.
+    
+    Args:
+        dfs_site (str): Abbreviation for each daily fantasy site to include in search.
+            Acceptable values: 'dk': DraftKings, 'fd': FanDuel, 'yh': Yahoo! Sports.
+            Use a list to pass in multiple values.   
+        season_from (int): The season number to begin search. Seasons are expressed in whole years.
+        week_from (int): The week number to begin search.
+        season_to (int): Season number to search for data up to (inclusive, default is None)
+        week_to (int): The week number to search for data up to (inclusive, default is None)
+    
+    Returns:
+        List: A list of URLs with formatted query strings based on the arguments entered.
+
+    Examples:
+        Return a list of search URLs for week 1 of the 2014 season across DraftKings and FanDuel.
+
+        >>> games_to_search(dfs_site=['dk', 'fd'], season_from=2014, week_from=1)
+        ['http://rotoguru1.com/cgi-bin/fyday.pl?week=1&year=2014&game=dk&scsv=1',
+        'http://rotoguru1.com/cgi-bin/fyday.pl?week=1&year=2014&game=fd&scsv=1']
+    """
 
     season_to_range = season_to
     week_to_range = week_to
@@ -25,6 +49,7 @@ def games_to_search(dfs_site, season_from, week_from, season_to=None, week_to=No
     game_urls = [base_url.format(w, s, g) for w, s, g in itertools.product(weeks, seasons, games)]
 
     return game_urls
+
 
 
 # Function to take game_urls and return data
