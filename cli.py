a8cf7d547c93890b2175b805ae7b966a010a2546
Brian <doucetba@gmail.com>
2020-04-06 21:48:49 -0400

    Fix to CLI

--- cli.py
+++ cli.py
@@ -9,15 +9,9 @@ print('Welcome to nfldfs!\n')
 
 # Options for command line interface
 @click.command()
-<<<<<<< HEAD
 @click.argument('dfs_site', nargs=-1)
 #@click.option('--dfs_site', required=True, prompt='Which dfs site(s)?', help='Name of the DFS site')
 @click.option('--season_from', required=True, type=int, prompt='Season beginning', help='The season number in the beginning search range')
-=======
-@click.option('--dfs_site', type=str, prompt='Which dfs site(s)?', help='Name of the DFS site')
-@click.option('--season_from',type=int, prompt='Season beginning', help='The season numnber in the beginning search range')
-@click.option('--week_from', type=int, prompt='Week beginning')
->>>>>>> ec28fdd02b8895a8eba032a0236e028098979543
 @click.option('--season_to', type=int, prompt='Season ending')
 @click.option('--week_to', type=int, prompt='Week ending')
 @click.option('--filename', type=str, prompt='Output Filename')
@@ -25,14 +19,8 @@ def get_dfs_data(dfs_site, season_from, season_to, week_from, week_to, filename)
     """
     Simple program to scrape NFL daily fantasy stats.
     """
-<<<<<<< HEAD
     print(list(dfs_site))
     dfs_site = (dfs_site)
-=======
-    dfs_site = [dfs_site]
-
-    print(dfs_site, season_from, season_to, week_from, week_to, filename)
->>>>>>> ec28fdd02b8895a8eba032a0236e028098979543
     g = games.find_games(dfs_site=dfs_site,
                          season_from=season_from,
                          week_from=week_from,
