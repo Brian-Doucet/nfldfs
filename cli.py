6c1e8de5ca4316e2bd8eed127db680f6ce82429a
Brian <doucetba@gmail.com>
2020-04-06 21:26:48 -0400

    Added argument and fixed print message

--- cli.py
+++ cli.py
@@ -5,12 +5,12 @@ from nfldfs import games as games
 
 
 # Welcome message
-print('Welcome to nfldfs!')
-print()
+print('Welcome to nfldfs!\n')
 
 # Options for command line interface
 @click.command()
-@click.option('--dfs_site', required=True, prompt='Which dfs site(s)?', help='Name of the DFS site')
+@click.argument('dfs_site', nargs=-1)
+#@click.option('--dfs_site', required=True, prompt='Which dfs site(s)?', help='Name of the DFS site')
 @click.option('--season_from', required=True, type=int, prompt='Season beginning', help='The season number in the beginning search range')
 @click.option('--season_to', type=int, prompt='Season ending')
 @click.option('--week_from', required=True, type=int, prompt='Week beginning')
@@ -21,7 +21,8 @@ def get_dfs_data(dfs_site, season_from, season_to, week_from, week_to, filename)
     """
     Simple program to scrape NFL daily fantasy stats.
     """
-    dfs_site = [dfs_site]
+    print(list(dfs_site))
+    dfs_site = (dfs_site)
     g = games.find_games(dfs_site=dfs_site,
                          season_from=season_from,
                          week_from=week_from,
