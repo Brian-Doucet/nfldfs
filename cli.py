0f007c9988c8b136a677679032a9f4c565b2accb
Brian <doucetba@gmail.com>
2020-04-08 09:02:01 -0400

    Fixed argument to accept multiple dfs sites Added help text for each option Updated the function docstring

--- cli.py
+++ cli.py
@@ -4,23 +4,42 @@ import click
 from nfldfs import games as games
 
 
-# Welcome message
-print('Welcome to nfldfs!\n')
-
 # Options for command line interface
 @click.command()
 @click.argument('dfs_site', nargs=-1)
-#@click.option('--dfs_site', required=True, prompt='Which dfs site(s)?', help='Name of the DFS site')
-@click.option('--season_from', required=True, type=int, prompt='Season beginning', help='The season number in the beginning search range')
-@click.option('--season_to', type=int, prompt='Season ending')
-@click.option('--week_to', type=int, prompt='Week ending')
-@click.option('--filename', type=str, prompt='Output Filename')
+@click.option('--season_from',
+              type=int,
+              prompt='Season beginning',
+              help='The season number in the beginning search range')
+@click.option('--season_to',
+              type=int,
+              prompt='Season ending',
+              help='The season number at the end of the search range (inclusive)')
+@click.option('--week_from',
+              type=int,
+              prompt='Week beginning',
+              help='The week number in the beginning search range')
+@click.option('--week_to',
+              type=int,
+              prompt='Week ending',
+              help='The week number at the end of the search range (inclusive)')
+@click.option('--filename',
+              type=str,
+              prompt='Enter a filename',
+              help='Filename for your results. Omit the .csv file extension')
+
 def get_dfs_data(dfs_site, season_from, season_to, week_from, week_to, filename):
     """
-    Simple program to scrape NFL daily fantasy stats.
+    Simple program to scrape NFL daily fantasy points and salary information.
+    Designed to be used as a bulk download tool. Results are returned in comma
+    delimited format (csv) to the /data directory. Check there to look at the
+    'draftkings_sample_output.csv'.
+
+    DFS_SITE The name of the dfs site(s) to return data for. Multiple sites should
+    be separated by a space. Refer to the package docs for more usage examples.
+
     """
-    print(list(dfs_site))
-    dfs_site = (dfs_site)
+    dfs_site = list(dfs_site)
     g = games.find_games(dfs_site=dfs_site,
                          season_from=season_from,
                          week_from=week_from,
