adbf5a4a6e64445fa7e1798fdfa7a24cc709a7c5
Ilya Yudkovich <iyudkovich@kyru.us>
2020-04-09 11:28:44 -0400

    show coverage report in terminal as well

--- tests.sh
+++ tests.sh
@@ -1,1 +1,1 @@
-pytest nfldfs  --cov-report xml:nfldfs/coverage.xml --cov=nfldfs --cov-config=.coveragerc
+pytest nfldfs  --cov-report xml:nfldfs/coverage.xml --cov-report term --cov=nfldfs --cov-config=.coveragerc
